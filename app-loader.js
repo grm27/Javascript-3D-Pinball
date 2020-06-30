b6066d7cea9806d50ed15731d7a19cc1688a3c9d
Raul <gabrielraul.marini@mail.polimi.it>
2020-06-30 18:14:32 +0200

    scene graphs already done

--- app-loader.js
+++ app-loader.js
@@ -1,6 +1,7 @@
 var program;
 var gl;
 let modelList = [];
+let graph;
 var modelStr = 'assets/objects/Body.obj';
 var modelTexture = 'assets/StarWarsPinball.png';
 
@@ -18,7 +19,6 @@ async function init() {
         let vertexShader = utils.createShader(gl, gl.VERTEX_SHADER, shaderText[0]);
         let fragmentShader = utils.createShader(gl, gl.FRAGMENT_SHADER, shaderText[1]);
         program = utils.createProgram(gl, vertexShader, fragmentShader);
-
     });
     gl.useProgram(program);
 
@@ -51,40 +51,137 @@ function initGlContext() {
     gl.enable(gl.DEPTH_TEST);
 }
 
-function loadModelProperties() {
+function buildSceneGraph() {
+
+    //define local matrices of each node
+    let bodyNode = new Node();
+    bodyNode.drawInfo = loadModelProperties(objectIndex.BODY);
+    bodyNode.localMatrix = localMatrices[objectIndex.BODY];
+
+    let ballNode = new Node();
+    ballNode.drawInfo = loadModelProperties(objectIndex.BALL)
+    ballNode.localMatrix = localMatrices[objectIndex.BALL];
+
+    let dl1Node = new Node();
+    dl1Node.drawInfo = loadModelProperties(objectIndex.DL1)
+    dl1Node.localMatrix = localMatrices[objectIndex.DL1];
+
+    let dl2Node = new Node();
+    dl2Node.drawInfo = loadModelProperties(objectIndex.DL2)
+    dl2Node.localMatrix = localMatrices[objectIndex.DL2];
+
+    let dl3Node = new Node();
+    dl3Node.drawInfo = loadModelProperties(objectIndex.DL3)
+    dl3Node.localMatrix = localMatrices[objectIndex.DL3];
+
+    let dl4Node = new Node();
+    dl4Node.drawInfo = loadModelProperties(objectIndex.DL4)
+    dl4Node.localMatrix = localMatrices[objectIndex.DL4];
+
+    let dl5Node = new Node();
+    dl5Node.drawInfo = loadModelProperties(objectIndex.DL5)
+    dl5Node.localMatrix = localMatrices[objectIndex.DL5];
+
+    let dl6Node = new Node();
+    dl6Node.drawInfo = loadModelProperties(objectIndex.DL6)
+    dl6Node.localMatrix = localMatrices[objectIndex.DL6];
+
+    let dr1Node = new Node();
+    dr1Node.drawInfo = loadModelProperties(objectIndex.BALL)
+    dr1Node.localMatrix = localMatrices[objectIndex.DR1];
+
+    let dr2Node = new Node();
+    dr2Node.drawInfo = loadModelProperties(objectIndex.DR2)
+    dr2Node.localMatrix = localMatrices[objectIndex.DR2];
+
+    let dr3Node = new Node();
+    dr3Node.drawInfo = loadModelProperties(objectIndex.DR3)
+    dr3Node.localMatrix = localMatrices[objectIndex.DR3];
+
+    let dr4Node = new Node();
+    dr4Node.drawInfo = loadModelProperties(objectIndex.DR4)
+    dr4Node.localMatrix = localMatrices[objectIndex.DR4];
+
+    let dr5Node = new Node();
+    dr5Node.drawInfo = loadModelProperties(objectIndex.DR5)
+    dr5Node.localMatrix = localMatrices[objectIndex.DR5];
+
+    let dr6Node = new Node();
+    dr6Node.drawInfo = loadModelProperties(objectIndex.DR6)
+    dr6Node.localMatrix = localMatrices[objectIndex.DR6];
+
+    let leftFlipperNode = new Node();
+    leftFlipperNode.drawInfo = loadModelProperties(objectIndex.LEFT_FLIPPER)
+    leftFlipperNode.localMatrix = localMatrices[objectIndex.LEFT_FLIPPER];
+
+    let rightFlipperNode = new Node();
+    rightFlipperNode.drawInfo = loadModelProperties(objectIndex.RIGHT_FLIPPER)
+    rightFlipperNode.localMatrix = localMatrices[objectIndex.RIGHT_FLIPPER];
+
+    let leftButtonNode = new Node();
+    leftButtonNode.drawInfo = loadModelProperties(objectIndex.LEFT_BUTTON)
+    leftButtonNode.localMatrix = localMatrices[objectIndex.LEFT_BUTTON];
+
+    let rightButtonNode = new Node();
+    rightButtonNode.drawInfo = loadModelProperties(objectIndex.RIGHT_BUTTON)
+    rightButtonNode.localMatrix = localMatrices[objectIndex.RIGHT_BUTTON];
+
+    let pullerNode = new Node();
+    pullerNode.drawInfo = loadModelProperties(objectIndex.PULLER)
+    pullerNode.localMatrix = localMatrices[objectIndex.PULLER];
+
+    let bumper1Node = new Node();
+    bumper1Node.drawInfo = loadModelProperties(objectIndex.BUMPER1)
+    bumper1Node.localMatrix = localMatrices[objectIndex.BUMPER1];
+
+    let bumper2Node = new Node();
+    bumper2Node.drawInfo = loadModelProperties(objectIndex.BUMPER2)
+    bumper2Node.localMatrix = localMatrices[objectIndex.BUMPER2];
+
+    let bumper3Node = new Node();
+    bumper3Node.drawInfo = loadModelProperties(objectIndex.BUMPER3)
+    bumper3Node.localMatrix = localMatrices[objectIndex.BUMPER3];
+
+    graph = [
+        ballNode,
+        dl1Node,
+        dl2Node,
+        dl3Node,
+        dl4Node,
+        dl5Node,
+        dl6Node,
+        dr1Node,
+        dr2Node,
+        dr3Node,
+        dr4Node,
+        dr5Node,
+        dr6Node,
+        leftFlipperNode,
+        rightFlipperNode,
+        leftButtonNode,
+        rightButtonNode,
+        pullerNode,
+        bumper1Node,
+        bumper2Node,
+        bumper3Node
+    ];
+
+    //define relationships
+    graph.forEach(function (node) {
+        node.setParent(bodyNode);
+    })
+}
+
+
+function loadModelProperties(index) {
 
     //Here we extract the position of the vertices, the normals, the indices, and the uv coordinates
     return {
-        tableVertices: modelList[objectIndex.BALL].vertices,
-        tableNormals: modelList[objectIndex.BALL].vertexNormals,
-        tableIndices: modelList[objectIndex.BALL].indices,
-        tableTexCoords: modelList[objectIndex.BALL].textures
+        vertices: modelList[index].vertices,
+        normals: modelList[index].vertexNormals,
+        indices: modelList[index].indices,
+        texCords: modelList[index].textures
     }
-}
-
-function buildSceneGraph() {
-
-    let ballNode = new Node();
-    let dl1Node = new Node();
-    let dl2Node = new Node();
-    let dl3Node = new Node();
-    let dl4Node = new Node();
-    let dl5Node = new Node();
-    let dl6Node = new Node();
-    let dr1Node = new Node();
-    let dr2Node = new Node();
-    let dr3Node = new Node();
-    let dr4Node = new Node();
-    let dr5Node = new Node();
-    let dr6Node = new Node();
-    let leftFlipperNode = new Node();
-    let rightFlipperNode = new Node();
-    let leftButtonNode = new Node();
-    let rightButtonNode = new Node();
-    let pullerNode = new Node()
-    let bumper1Node = new Node();
-    let bumper2Node = new Node();
-    let bumper3Node = new Node();
 }
 
 function loadGlslProperties() {
@@ -103,6 +200,10 @@ function bindModelProperties(objProp, glslProperties) {
     let perspectiveMatrix = utils.MakePerspective(120, gl.canvas.width / gl.canvas.height, 0.1, 100.0);
     let viewMatrix = utils.MakeView(0.0, 0.0, 0.0, 0.0, 0.0);
 
+
+    graph.forEach(function (node) {
+
+    })
     let vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
 
